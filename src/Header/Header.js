7a1ec7a5d53da4083c3045f2a461c90ebe7137f5
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-03 18:56:45 +0200

    feat: header сomplete

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Tabs, Tab, Button, Link } from '@mui/material';
+import CycloneIcon from '@mui/icons-material/Cyclone';
+
+const links = ['Products', 'Solutions', 'Pricing', 'Enterprise'];
 
 const Header = () => {
   return (
-    <AppBar position='fixed' color='primary'>
+    <AppBar position='fixed' sx={{ bgcolor: 'transparent', boxShadow: '0' }}>
       <Toolbar>
-        <Typography variant='h6'>Typography</Typography>
+        <Box sx={{ display: 'flex', width: '100%', alignItems: 'center' }}>
+          <CycloneIcon fontSize='large' sx={{ color: 'black' }} />
+
+          <Box>
+            <Typography variant='h6' sx={{ margin: 2, color: 'black' }}>
+              MUI Project
+            </Typography>
+          </Box>
+
+          <Box>
+            <Tabs component={Link} sx={{ textDecoration: 'none' }}>
+              {links.map((link, index) => (
+                <Tab
+                  sx={{
+                    fontWeight: 'bold',
+                    textDecoration: 'none',
+                    ':hover': { textDecoration: 'underline', textUnderlineOffset: '5px' },
+                  }}
+                  key={index}
+                  label={link}
+                />
+              ))}
+            </Tabs>
+          </Box>
+
+          <Box display='flex' marginLeft={'auto'}>
+            <Button sx={{ margin: 1 }} variant='outlined'>
+              Talk To Us
+            </Button>
+            <Button sx={{ margin: 1 }} variant='contained'>
+              Try For Free
+            </Button>
+          </Box>
+        </Box>
       </Toolbar>
     </AppBar>
   );
