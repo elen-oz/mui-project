54ac455ccb048a9d18e09d1f59ee6aa8c86c9c1c
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-04 00:34:45 +0200

    feat: responsive header

---
+++ src/Header/DrawerComponent.js
@@ -0,0 +1,28 @@
+import { React, useState } from 'react';
+import { Drawer, List, ListItemButton, ListItemText, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const sampleList = ['products', 'services'];
+
+const DrawerComponent = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <List>
+          {sampleList.map((item) => (
+            <ListItemButton>
+              <ListItemText>{item}</ListItemText>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton onClick={() => setOpen(true)} sx={{ ml: 'auto' }}>
+        <MenuIcon color='black' />
+      </IconButton>
+    </>
+  );
+};
+
+export default DrawerComponent;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,48 +1,72 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box, Tabs, Tab, Button, Link } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Tabs,
+  Tab,
+  Button,
+  Link,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import CycloneIcon from '@mui/icons-material/Cyclone';
+import DrawerComponent from './DrawerComponent';
 
 const links = ['Products', 'Solutions', 'Pricing', 'Enterprise'];
 
 const Header = () => {
+  const isMatch = useMediaQuery(useTheme().breakpoints.down('md'));
+  console.log(isMatch);
+
   return (
-    <AppBar position='sticky' sx={{ bgcolor: 'transparent', boxShadow: '0' }}>
-      <Toolbar>
-        <Box sx={{ display: 'flex', width: '100%', alignItems: 'center' }}>
+    <AppBar position='sticky' sx={{ padding: 1, bgcolor: 'transparent', boxShadow: '0' }}>
+      {isMatch ? (
+        <Box display='flex' sx={{ alignItems: 'center' }}>
           <CycloneIcon fontSize='large' sx={{ color: 'black' }} />
-
-          <Box>
-            <Typography variant='h6' sx={{ margin: 2, color: 'black' }}>
-              MUI Project
-            </Typography>
-          </Box>
-
-          <Box>
-            <Tabs component={Link} sx={{ textDecoration: 'none' }}>
-              {links.map((link, index) => (
-                <Tab
-                  sx={{
-                    fontWeight: 'bold',
-                    textDecoration: 'none',
-                    ':hover': { textDecoration: 'underline', textUnderlineOffset: '5px' },
-                  }}
-                  key={index}
-                  label={link}
-                />
-              ))}
-            </Tabs>
-          </Box>
-
-          <Box display='flex' marginLeft={'auto'}>
-            <Button sx={{ mr: 2 }} variant='outlined'>
-              Talk To Us
-            </Button>
-            <Button sx={{ ml: 2 }} variant='contained'>
-              Try For Free
-            </Button>
-          </Box>
+          <Typography variant='h6' sx={{ padding: 2, color: 'black' }}>
+            MUI
+          </Typography>
+          <DrawerComponent />
         </Box>
-      </Toolbar>
+      ) : (
+        <Toolbar>
+          <Box sx={{ display: 'flex', width: '100%', alignItems: 'center' }}>
+            <CycloneIcon fontSize='large' sx={{ color: 'black' }} />
+
+            <Box>
+              <Typography variant='h6' sx={{ padding: 2, color: 'black' }}>
+                MUI
+              </Typography>
+            </Box>
+
+            <Box>
+              <Tabs component={Link} sx={{ textDecoration: 'none' }}>
+                {links.map((link, index) => (
+                  <Tab
+                    sx={{
+                      textDecoration: 'none',
+                      ':hover': { textDecoration: 'underline', textUnderlineOffset: '5px' },
+                    }}
+                    key={index}
+                    label={link}
+                  />
+                ))}
+              </Tabs>
+            </Box>
+
+            <Box display='flex' marginLeft={'auto'}>
+              <Button sx={{ mr: 1 }} variant='outlined'>
+                Talk To Us
+              </Button>
+              <Button sx={{ ml: 1 }} variant='contained'>
+                Try For Free
+              </Button>
+            </Box>
+          </Box>
+        </Toolbar>
+      )}
 
       <Box width='100%' height='100vh'>
         <video
