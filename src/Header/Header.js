0d0ec95e7dd6659a3847a51f7aedf4bd9cc98798
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-03 22:13:36 +0200

    feat: add animation, text and button

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -34,15 +34,40 @@ const Header = () => {
           </Box>
 
           <Box display='flex' marginLeft={'auto'}>
-            <Button sx={{ margin: 1 }} variant='outlined'>
+            <Button sx={{ mr: 2 }} variant='outlined'>
               Talk To Us
             </Button>
-            <Button sx={{ margin: 1 }} variant='contained'>
+            <Button sx={{ ml: 2 }} variant='contained'>
               Try For Free
             </Button>
           </Box>
         </Box>
       </Toolbar>
+
+      <Box width='100%' height='100vh'>
+        <video
+          width={'100%'}
+          height={'70%'}
+          autoPlay
+          loop
+          muted
+          playsInline
+          src='/video.mp4'
+        ></video>
+        <Box display='flex' width='100%'>
+          <Typography margin='auto' variant='h4' color='black' textAlign={'center'}>
+            Build Your Hassle Free And With Top Noch Quality
+          </Typography>
+        </Box>
+        <Box width='100%' display='flex' justifyContent={'center'} margin='auto' marginTop={5}>
+          <Button variant='outlined' sx={{ mr: 3 }}>
+            Signuo With Email
+          </Button>
+          <Button variant='contained' sx={{ ml: 3 }}>
+            Signuo With Google
+          </Button>
+        </Box>
+      </Box>
     </AppBar>
   );
 };
